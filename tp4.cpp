c5778a049eabcb31599122a72307e2cbca40317c
FNMariani <facundonicolasm@gmail.com>
2020-05-02 13:48:39 -0300

    Buscar function for "word"

--- tp4.cpp
+++ tp4.cpp
@@ -12,6 +12,7 @@ typedef struct {
 void CargarTareas(Tarea** tareas, int cantidad);
 void ListarTareas(Tarea** tareas, Tarea** tareasRealizadas, int cantidad);
 void MostrarTareas(Tarea** tareas, int cantidad);
+Tarea BuscarTarea(Tarea** tareas, Tarea** tareasRealizadas, int cantidad);
 
 int main()
 {
@@ -21,6 +22,7 @@ int main()
 
     printf("Ingrese cantidad de tareas a cargar: ");
     scanf("%d", &cantidad_tareas);
+    fflush(stdin);
 
     tareas = (Tarea**) malloc(cantidad_tareas * sizeof(Tarea*));
     tareasRealizadas = (Tarea**) malloc(cantidad_tareas * sizeof(Tarea*));
@@ -30,11 +32,20 @@ int main()
     ListarTareas(tareas, tareasRealizadas, cantidad_tareas);
     system("cls");
 
+    printf("/////////////////////////////////////////////////////\n");
     printf("Tareas Realizadas: \n");
     MostrarTareas(tareasRealizadas, cantidad_tareas);
 
-    printf("\nTarea Pendientes: \n");
+    printf("\n/////////////////////////////////////////////////////\n");
+    printf("Tarea Pendientes: \n");
     MostrarTareas(tareas, cantidad_tareas);
+
+    printf("\n/////////////////////////////////////////////////////\n");
+    Tarea aux;
+    aux = BuscarTarea(tareas, tareasRealizadas, cantidad_tareas);
+    printf("ID: %d\n", aux.TareaID);
+    printf("Descripcion: %s\n", aux.Descripcion);
+    printf("Duracion: %d\n\n", aux.Duracion);
     
     getchar();
     system("PAUSE");
@@ -50,7 +61,7 @@ void CargarTareas(Tarea** tareas, int cantidad)
         tareas[i]->TareaID = i+1;
 
         printf("Ingrese la descripcion de la tarea: ");
-        scanf("%s", descrip);
+        gets(descrip);
         tareas[i]->Descripcion = (char*) malloc(strlen(descrip) * sizeof(char));
         strcpy(tareas[i]->Descripcion, descrip);
 
@@ -67,12 +78,12 @@ void ListarTareas(Tarea** tareas, Tarea** tareasRealizadas, int cantidad)
     for (int i = 0; i < cantidad; i++)
     {
         printf("ID: %d\n", tareas[i]->TareaID);
-        printf("Nombre: %s\n", tareas[i]->Descripcion);
+        printf("Descripcion: %s\n", tareas[i]->Descripcion);
         printf("Duracion: %d\n\n", tareas[i]->Duracion);
 
-        fflush(stdin);
         printf("La tarea fue realizada? (S o N) ");
         scanf("%c", &resp);
+        fflush(stdin);
 
         if(tolower(resp) == 's')
         {
@@ -94,8 +105,32 @@ void MostrarTareas(Tarea** tareas, int cantidad)
         if(tareas[i])
         {
             printf("ID: %d\n", tareas[i]->TareaID);
-            printf("Nombre: %s\n", tareas[i]->Descripcion);
+            printf("Descripcion: %s\n", tareas[i]->Descripcion);
             printf("Duracion: %d\n\n", tareas[i]->Duracion);
         }
     }
+}
+
+Tarea BuscarTarea(Tarea** tareas, Tarea** tareasRealizadas, int cantidad)
+{
+    char palabra[20];
+
+    printf("Ingrese la palabra a buscar: ");
+    gets(palabra);
+
+    for (int i = 0; i < cantidad; i++)
+    {
+        if(tareas[i] && (strstr(tareas[i]->Descripcion, palabra) != NULL) )
+        {
+            return *tareas[i];
+        }
+        if(tareasRealizadas[i] && (strstr(tareasRealizadas[i]->Descripcion, palabra) != NULL) )
+        {
+            return *tareasRealizadas[i];
+        }
+    }
+
+    Tarea nula;
+    nula.TareaID = 0;
+    return nula;
 }
